23c96c1c301807d7d41173bca13e1aa0180f0b2a
saymonm_set <saymonm_set@hotmail.com>
2018-09-19 12:05:43 -0400

    Para tenerlo como servicio json

--- src/main/java/com/apress/spring/controller/JournalController.java
+++ src/main/java/com/apress/spring/controller/JournalController.java
@@ -1,13 +1,19 @@
 package com.apress.spring.controller;
 
 
+import com.apress.spring.domain.JournalEntry;
 import
         org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import       org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import       org.springframework.web.bind.annotation.RestController;
 import       org.springframework.web.servlet.ModelAndView;
 import com.apress.spring.repository.JournalRepository;
+
+import java.util.List;
+
 /**
  * Created by simon on 22/08/18.
  */
@@ -21,5 +27,11 @@ public class JournalController {
         modelAndView.setViewName(VIEW_INDEX);
         modelAndView.addObject("journal", repo.findAll());
         return modelAndView;
+    }
+
+    @RequestMapping(value="/journal", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public @ResponseBody
+    List<JournalEntry> getJournal(){
+        return repo.findAll();
     }
 }
